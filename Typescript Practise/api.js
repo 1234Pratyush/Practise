4e4ed8b2ddb51d3a6ab36b4379d5575c5b13fde5
Pratyush <pratyushnegi58@gmail.com>
2026-06-25 16:20:46 +0530

    Ts Practise

---
+++ Typescript Practise/api.js
@@ -0,0 +1,5 @@
+"use strict";
+const name1 = "Hello";
+console.log(name1);
+const value2 = 34;
+console.log(value2);
---
+++ Typescript Practise/api.ts
@@ -0,0 +1,6 @@
+const name1:string ="Hello";
+console.log(name1);
+
+const value2:number=34;
+console.log(value2);
+
