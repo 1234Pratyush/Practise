0129ea42503aa78bfb58610ecdd242070cdd7537
Pratyush <pratyushnegi58@gmail.com>
2026-07-28 18:34:30 +0530

    string  in python

--- Python Practise/one.py
+++ Python Practise/one.py
@@ -1,83 +1,32 @@
-# print("Hello worlds","567")
-# print(34)
-# print(45+44)
-# print(10%2)
+# String in python
 
-# age = 34
-# name  = "Pratyush"
+# str1 = "this is a string"
+# str2 = "example"
+# print(str1 + " " + str2)
+# print(len(str1))
+# value3 = str2[5]
+# print(value3)
 
-# price = 56.45
-# print("My name is : ",name,"and my age is :",age)
-# print(type(price))
-# print(type(age))
-# print(type(name))
+# indexing in python
 
 
+# print(len(str))
+# str = "Apple"
+# print(str[-5:-1])
+
+# str="i am studying python"
+# print(str.endswith("app"))
+# print(str.capitalize())
+# print(str.replace("python","javascript"))
+# print(str.find("hello"))
+# print(str.count("a"))
+
+# name = input("name :")
+
+# print(len(name))
 
 
-# a = 12  
-# b = -5
-# c = a%b
-# print(c)
-
-# name = input("name : ")
-# print(name)
-
-# light = input("light : ")
-# if(light == "green"):
-#     print("go")
-# elif(light == "red"):
-#     print("stop")    
-# elif(light == "yellow"):
-#     print("look")    
-# else:
-#     print("light is broken")
+str2="Hi $Iam the $ symbol $99.90"
+print("$ is ocuuring",str2.count("$"),"times")
 
 
-# marks = int(input("marks :"))
-# if(marks >= 90):
-#     print("A")
-# elif(marks >= 95):
-#     print("A+")
-# elif(marks == 100):
-#     print("O")
-# else:
-#     print("Student is failed")            
-
-
-
-# food = input("food :")
-# if food == "cake" | food == "jalebi":
-#     print("Sweet")
-# else:
-#     print("Not Sweet")    
-
-# username = input("username : ")
-# password = int(input("password :"))
-
-# if username == "pratyush" and password == 4321:
-#     print("Welcome back pratyush,You are successfully logged in ")
-# else:
-#     print("Inavlid Crediantials")    
-
-# num1   = int(input("number 1 :"))
-# num2 = int(input("number 2 :"))
-
-# sum = num1 ** num2
-# print("Sum of two number is :",sum)
-
-
-# value1 = True   
-# print(type(value1))
-
-# side = float(input("Enter the side : "))
-
-# area  = side * side
-# print("Area of square is :" ,area)
-
-
-num1 = float(input("Enter first :"))
-num2 = float(input("Enter second :"))
-
-avg = (num1 + num2) / 2
-print("The average is:",avg)
