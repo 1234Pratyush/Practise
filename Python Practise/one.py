ced5813f67b5cc009a742a34783931d021a3123b
Pratyush <pratyushnegi58@gmail.com>
2026-07-29 17:03:39 +0530

    practise

--- Python Practise/one.py
+++ Python Practise/one.py
@@ -1,46 +1,6 @@
-# String in python
+# List and tuples
 
-# str1 = "this is a string"
-# str2 = "example"
-# print(str1 + " " + str2)
-# print(len(str1))
-# value3 = str2[5]
-# print(value3)
+marks = [86,56,76,45,32]
+print(marks[0:8])
 
-# indexing in python
-
-
-# print(len(str))
-# str = "Apple"
-# print(str[-5:-1])
-
-# str="i am studying python"
-# print(str.endswith("app"))
-# print(str.capitalize())
-# print(str.replace("python","javascript"))
-# print(str.find("hello"))
-# print(str.count("a"))
-
-# name = input("name :")
-
-# print(len(name))
-
-
-# str2="Hi $Iam the $ symbol $99.90"
-# print("$ is ocuuring",str2.count("$"),"times")
-
-
-num1 = int(input("enter your first number:"))
-num2 = int(input("enter your first number:"))
-num3 = int(input("enter your first number:"))
-
-if(num1 > num2 and num1 > num3):
-    print("num1 is greater")  
-elif(num2 > num1 and num2 > num3):
-    print("num2 is greater")
-else:
-    print("num3 is greater")
-
-
-
-
+print(marks[-5:-2])
