feb32b87e15e02bb1764e1005400bd8084a88d37
Pratyush <pratyushnegi58@gmail.com>
2026-07-30 11:02:56 +0530

    setup the react tailiwindCss with redux and created the product page

---
+++ React-Redux/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Products from "./pages/Products";
+import { Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <div className="min-h-screen bg-zinc-900 text-white">
+     
+      <Routes>
+        <Route path="/" element={<Products />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ React-Redux/vite-project/src/pages/Products.jsx
@@ -0,0 +1,40 @@
+import  { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "../components/Card";
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get("https://dummyjson.com/products");
+        setProducts(response.data.products);
+        console.log(response.data.products);
+      } catch (error) {
+        console.log("ERROR :", error.message);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+ return (
+   <div className="mx-auto max-w-7xl px-8">
+     <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+       {products.map((product) => (
+         <Card
+           key={product.id}
+           image={product.images[0]}
+           category={product.category}
+           price={product.price}
+           rating={product.rating}
+           title={product.title}
+           description={product.description}
+         />
+       ))}
+     </div>
+   </div>
+ );
+  ;
+};
+
+export default Products;
