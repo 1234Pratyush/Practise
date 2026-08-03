bad0a670568f2120cc6d54163ee877a89cd2c30c
Pratyush <pratyushnegi58@gmail.com>
2026-08-03 11:19:03 +0530

    js practise

---
+++ Js Practise/practise/one.js
@@ -0,0 +1,35 @@
+// let num = 25;
+
+// let userNum = prompt("Guess a number : ");
+
+// while(userNum != num){
+//       userNum = prompt("Sorry you entered the wrong number,Guess again : ")
+// }
+
+// console.log("Congratualations you have won")
+
+
+// string in js
+
+let str = "helllo byy"
+console.log(typeof(str));
+
+let num = 24;
+console.log(typeof(num))
+// console.log(num + str)
+
+ if(str.length > 20){
+    console.log("this is much bigger string")
+ }
+
+ console.log("Short string")
+ console.log(str.length)
+
+
+let obj ={
+    name :"pen",
+    price:10,
+}
+
+let output = `this is my ${obj.name} and is of ${obj.price} ruppes`
+console.log(output)
---
+++ Js Practise/two.js
@@ -0,0 +1,16 @@
+// // print sum 1 to 5
+// let value = 5;
+// let sum = 0;
+// for (let i = 1; i <= value; i++) {
+//  console.log(i)
+// }
+
+
+//while loop 
+let num = 100
+for(let i = 0;i<=num;i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
+
