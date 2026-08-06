ec899fbd22962b36b95b89f600590f7faec09ae9
Pratyush <pratyushnegi58@gmail.com>
2026-08-06 11:57:47 +0530

    practise

--- Js Practise/practise/function.js
+++ Js Practise/practise/function.js
@@ -1,8 +1,24 @@
-let nums = ["Hello","Byyy","Tata","Punch"];
+// let nums = ["Hello","Byyy","Tata","Punch"];
 
-let output =  nums.map((value)=>{
-  console.log(value.toLocaleUpperCase())
+// let output =  nums.map((value)=>{
+//   console.log(value.toLocaleUpperCase())
+// })
+
+// console.log(output)
+// console.log(nums)
+
+let arr = [1,2,3,4,5,6,7,8];
+
+let evenArr = arr.filter((value)=>{
+      return   value % 2 == 0;
 })
 
+console.log(evenArr);
+console.log(arr);
+
+const output =  arr.reduce((res,curr)=>{
+  return res + curr
+})
+
+
 console.log(output)
-console.log(nums)
