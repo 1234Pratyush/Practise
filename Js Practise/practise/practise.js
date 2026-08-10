292d173c6f99214dc761a62ca5fee6818f6727cc
Pratyush <pratyushnegi58@gmail.com>
2026-08-10 10:55:52 +0530

    practise

--- Js Practise/practise/practise.js
+++ Js Practise/practise/practise.js
@@ -1,2 +1,9 @@
 let heading = document.getElementById("heading1");
 console.dir(heading);
+
+
+let headings = document.getElementsByClassName("headings");
+console.log(headings);
+
+let paras = document.getElementsByTagName("p")
+console.log(paras)
